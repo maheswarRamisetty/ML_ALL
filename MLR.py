0febf7d4af1e28f58a67e65d146fee456a485b45
maheswarRamisetty <maheshwarramisetty184@gmail.com>
2026-01-29 10:36:23 +0530

    NP and PD

---
+++ MLR.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+values = [20.1, 20.8, 21.9, 22.5, 22.7, 22.3, 21.8, 21.2, 20.9, 2,0.1]
+c = np.array(values)
+# plt.plot(c)
+# plt.show()
+
+x = np.array([ [67, 63, 87],
+[77, 69, 59],
+[85, 87, 99],
+[79, 72, 71],
+[63, 89, 93],
+[68, 92, 78]])
+
+# x.shape = (9,2)
+
+# print(x)
+
+# A=np.array([1,2,3,4,5])
+# S=A[2:]
+# print(np.may_share_memory(A,S))
+
